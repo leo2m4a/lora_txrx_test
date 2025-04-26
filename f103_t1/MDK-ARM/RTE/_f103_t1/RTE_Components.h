9f405400114253253e1de26e739e496c7a09d938
leowang <leo2m4a@yahoo.com.tw>
2025-04-26 18:22:23 +0800

    add

---
+++ f103_t1/MDK-ARM/RTE/_f103_t1/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'f103_t1' 
+ * Target:  'f103_t1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
